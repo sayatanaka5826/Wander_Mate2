88c01010168e50d98bf67f91ae7ee89b288a0a4a
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-07 21:06:37 +0000

    マイページ編集画面作成中

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,2 +1,33 @@
 class User::UsersController < ApplicationController
+
+  def edit
+    @user = current_user
+    @gender_options = User.genders_i18n.invert.map{|key,value|[key,value]}
+    @age_options = User.ages_i18n.invert.map{|key,value|[key,value]}
+    @smoking_options = User.smokings_i18n.invert.map{|key,value|[key,value]}
+    @drinking_options = User.drinkings_i18n.invert.map{|key,value|[key,value]}
+    @favorite_area_options = User.favorite_areas_i18n.invert.map{|key,value|[key,value]}
+    @budget_options = User.budgets_i18n.invert.map{|key,value|[key,value]}
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:mypage_edit] = "プロフィールが更新されました。"
+      redirect_to my_page_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :bio, :gender, :age,
+    :smoking, :drinking, :country_count, :favorite_area,
+    :budget, :is_active, :profile_image)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,43 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
-  
+
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   has_many :entries, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :rooms, through: :entries
-  
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :bio, length: { maximum: 200 }
-  
-  
+
+  enum gender: {male: 0 , female: 1, other: 2}
+  enum age: {teens: 0 , twenties: 1, thirties: 2, forties: 3, fifties: 4,
+    sixties: 5, seventies: 6, older: 7}
+  enum smoking: {smoker: 0 , non_smoker: 1}
+  enum drinking: {drinker: 0 , non_drinker: 1}
+  enum favorite_area: {north_america: 0 , south_america: 1, europe: 2, africa: 3,
+    asia: 4, oceania: 5, others: 6}
+  enum budget: {low: 0 , normal: 1, high: 2}
+
+
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
