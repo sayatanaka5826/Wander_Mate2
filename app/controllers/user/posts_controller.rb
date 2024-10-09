3b0154f1afa4675f1fcf0b32465b5b1b7e2382d0
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-09 08:37:07 +0000

    プロフィール編集レイアウト調整

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,9 +1,9 @@
 class User::PostsController < ApplicationController
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -14,24 +14,26 @@ class User::PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
-  
+
+
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :body, :post_image)
   end
-  
+
 end
