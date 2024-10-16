d6824769a837913b70dd30a8767394a09f8b0f2b
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-16 09:53:38 +0000

    検索結果一覧ページネーション調整中

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -4,9 +4,9 @@ class User::SearchesController < ApplicationController
     @range = params[:range]
     @word = params[:word]
     if @range == "ユーザー"
-      @users = User.where("name LIKE ?","%#{@word}%").page(params[:page]).per(10)
+      @users = User.where("name LIKE ?","%#{@word}%").page(params[:page]).per(3)
     elsif @range == "投稿"
-      @posts = Post.where("title LIKE ?","%#{@word}%").page(params[:page]).per(10)
+      @posts = Post.where("title LIKE ?","%#{@word}%").page(params[:page]).per(3)
     end
   end
 
