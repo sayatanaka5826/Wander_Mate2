88d5f0b9e9343259cdabe818114a2c882c72a9e6
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-16 09:08:57 +0000

    検索機能実装中

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,2 +1,14 @@
 class User::SearchesController < ApplicationController
+
+  def search
+    @range = params[:range]
+    @word = params[:word]
+    if @range == "ユーザー"
+      @users = User.where("name LIKE ?","%#{@word}%").page(params[:page]).per(10)
+    elsif @range == "投稿"
+      @posts = Post.where("title LIKE ?","%#{@word}%").page(params[:page]).per(10)
+    end
+  end
+
+
 end
