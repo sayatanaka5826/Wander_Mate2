22d968bf84f03c501911cb084a8fdfb6b668364c
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-17 16:59:54 +0000

    admin検索機能、検索結果一覧作成

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,13 @@
 class Admin::SearchesController < ApplicationController
+  
+  def search
+    @range = params[:range]
+    @word = params[:word]
+    if @range == "ユーザー"
+      @users = User.where("name LIKE ?", "%#{@word}%").order(created_at: :desc).page(params[:page]).per(10)
+    elsif @range == "投稿"
+      @posts = Post.where("title LIKE ?","%#{@word}%").order(created_at: :desc).page(params[:page]).per(10)
+    end
+  end
+  
 end
