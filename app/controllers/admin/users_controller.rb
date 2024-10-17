209a68d4f37ee7c543142a7f97e08594e7e54cab
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-10-17 13:35:57 +0000

    adminユーザー一覧、詳細、退会機能作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,19 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc).page(params[:page]).per(10)
   end
-  
+
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(3)
   end
-  
+
   def destroy
     @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_top_path
   end
-  
+
 end
